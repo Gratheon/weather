8cc5686707b788225af7255f5ffcf2809198df64
Artjom Kurapov <artjom.kurapov@clarifai.com>
2023-08-24 01:53:38 +0300

    use --build at restart

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,6 @@ WORKDIR /app
 COPY . /app/
 RUN npm install
 
-
 EXPOSE 4000
 
 CMD ["node", "app/weather.js"]
--- restart.sh
+++ restart.sh
@@ -1,3 +1,3 @@
 cd /www/weather/
 COMPOSE_PROJECT_NAME=gratheon docker-compose down
-COMPOSE_PROJECT_NAME=gratheon docker-compose up -d
+COMPOSE_PROJECT_NAME=gratheon docker-compose up --build -d
